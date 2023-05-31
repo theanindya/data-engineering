d6e59c3993c9fb00f407ce522c885b9cfb36ba22
theanindya <40605723+theanindya@users.noreply.github.com>
2023-05-31 12:50:42 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,8 +1,10 @@
 # Execute the data pipeline
 python3 automated_pipeline.py
 
+database_file="dbanindya"
+
 # Check if the output file(s) exist
-if [ -f output_file.txt ]; then
+if [ -f database_file ]; then
     echo "Output file exists."
 else
     echo "Output file does not exist."
