07874373f935f3d6e84b81475cb51db4b52b56d3
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 12:54:46 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
-column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid',  'others']
 
 df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=6, skipfooter=4,
                  usecols=[0,1,2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python',
