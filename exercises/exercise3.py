71f45de1451c506c2e6649558bd54e87b6e3c101
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 13:03:03 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,14 +1,17 @@
 import pandas as pd 
 
-dataset_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
+db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
+column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
-
-df = pd.read_csv(dataset_url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
-                 usecols=[*range(0, 3), 12, 22, 32, 42, 52, 62, 72], names=columns, engine='python',
+df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=6, skipfooter=4,
+                 usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python',
                  converters={'CIN': str})
 
 df = df[df["petrol"].str.contains("-")==False]
 df = df.astype({'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'})
 
-df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
+engine = sa.create_engine("sqlite:///cars.sqlite")
+df.to_sql('cars', engine, if_exists='replace', index=False)
+
+
+
