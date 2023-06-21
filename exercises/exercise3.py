cff699401900e8d39654561b03824925b8a504ba
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 14:58:25 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -3,12 +3,17 @@ import pandas as pd
 db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=7, skipfooter=4,
+df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=6, skipfooter=4,
                  usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python',
                  converters={'CIN': str})
 
 df = df[df["petrol"].str.contains("-")==False]
 df = df[df['CIN'].str.contains(r'^\d{5}$', regex=True)]
+
+ df = df.dropna()
+x = df.iloc[:, 3:].gt(0).any(axis=1)
+df = df[x]
+
 df = df.astype({'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'})
 
 
