fbd2f371d1d5b9744d8dde40b79d0f7943a8ee04
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 15:13:54 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -3,9 +3,10 @@ import pandas as pd
 db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=range(1, 7), skipfooter=4,
+df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=range(0, 7), skipfooter=4,
                  usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python',
-                 converters={'CIN': str})
+                 na_values='.', thousands=',',
+                 dtype={'CIN': str})
 
 df = df[df["petrol"].str.contains("-")==False]
 
