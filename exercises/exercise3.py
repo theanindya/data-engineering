f2b464be58b840520cc1646c9f1bf8395bd66e51
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 15:06:10 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -3,7 +3,7 @@ import pandas as pd
 db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=7, skipfooter=4,
+df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', skiprows=range(1, 8), skipfooter=4,
                  usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python',
                  converters={'CIN': str})
 
@@ -17,6 +17,8 @@ df = df.astype({'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'in
 
 
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
+
+
 
 
 
