6f842bbb5eb251d5efa0af451bc7d63b1c35bc80
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 15:24:21 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 db = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
-column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid','others']
 
 df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=range(0, 7), skipfooter=4,
                  usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], names=column_names, engine='python', na_values='.', thousands=',',dtype={'CIN': str})
@@ -12,7 +12,8 @@ df = df[df['CIN'].str.contains(r'^\d{5}$', regex=True)]
 
 
 
-df = df.astype({'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'})
+df = df.astype({col: 'int64' for col in ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']})
+
 
 
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
