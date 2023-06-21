daf128b593f75d744bee1bb7fd58daa2d6118259
theanindya <40605723+theanindya@users.noreply.github.com>
2023-06-21 14:02:10 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -8,6 +8,7 @@ df = pd.read_csv(db, sep=';', encoding='ISO-8859-1', header=None, skiprows=7, sk
                  converters={'CIN': str})
 
 df = df[df["petrol"].str.contains("-")==False]
+df = df.dropna()
 df = df.astype({'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'})
 
 
