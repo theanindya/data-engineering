d1df4d39671f67527ec027b73ba44557a141e685
theanindya <40605723+theanindya@users.noreply.github.com>
2023-07-11 21:29:12 +0200

    Update exercise5.py

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -4,35 +4,99 @@ import zipfile
 import pandas as pd
 import sqlite3
 
-# Set the file paths
-db_path = "gtfs.sqlite"
-zip_file_path = "GTFS.zip"
-csv_file_path = "stops.txt"
+# Step 1: Download function to download the GTFS ZIP file
+def download_zip_file(url, file_path):
+    urllib.request.urlretrieve(url, file_path)
 
-# Step 1: Download the ZIP file
-download_zip_file("https://gtfs.rhoenenergie-bus.de/GTFS.zip", zip_file_path)
+# Step 2: Unzip function to extract stops.txt from the downloaded ZIP file
+def unzip_file(zip_path, extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extract('stops.txt', path=extract_path)
 
-# Step 2: Unzip the file and get the CSV file path
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall(".")
+# Step 3: Data processing function to reshape and transform the data
+def data_transformation():
+    # Read the stops.txt file
+    df = pd.read_csv("stops.txt")
 
-# Step 3: Read the CSV file and create a Pandas DataFrame
-df = pd.read_csv(csv_file_path, sep=',', encoding='UTF-8', header=0, usecols=[0, 2, 4, 5, 6],dtype={"stop_id": int, "stop_name": str, "stop_lat": float, "stop_lon":float, "zone_id": int})
+    # Select only the required columns
+    df = df[['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id']]
 
-# Step 4: Filter the data to only keep stops from zone 2001
-df = df[df['zone_id'] == 2001]
+    # Filter data for zone_id 2001
+    df = df[df['zone_id'] == 2001]
 
-# Step 5: Validate the data
-invalid_lats = df[df['stop_lat'] < -90]
-invalid_lons = df[df['stop_lon'] < -90]
-invalid_rows = invalid_lats.append(invalid_lons)
-df = df.drop(invalid_rows.index)
+    # Validate stop_name with German umlauts
+    df['stop_name'] = df['stop_name'].str.replace('ae', 'ä').replace('oe', 'ö').replace('ue', 'ü').replace('ss', 'ß')
 
-# Step 6: Write the data to the SQLite database
-conn = sqlite3.connect(db_path)
-df.to_sql('stops', conn, if_exists="replace", index=False)
-conn.commit()
-conn.close()
+    # Validate stop_lat/stop_lon as geographic coordinates
+    df = df[df['stop_lat'].between(-90, 90)]
+    df = df[df['stop_lon'].between(-90, 90)]
 
-# Print a message to indicate that the data pipeline has run successfully
-print("Data pipeline runs successfully.")
+    return df
+
+# Step 4: Data validation function
+def data_validations(df):
+    # Drop rows containing invalid data
+    df.dropna(inplace=True)
+
+    return df
+
+# Step 5: Function to write the data to the SQLite database
+def load_data(df, db_path, table_name):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Define the table name and schema
+    table_name = table_name
+    table_schema = [
+        ("stop_id", "BIGINT"),
+        ("stop_name", "TEXT"),
+        ("stop_lat", "FLOAT"),
+        ("stop_lon", "FLOAT"),
+        ("zone_id", "BIGINT")
+    ]
+
+    # Create the table
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+    for column, data_type in table_schema:
+        create_table_query += f"{column} {data_type}, "
+    create_table_query = create_table_query.rstrip(", ") + ")"
+
+    cursor.execute(create_table_query)
+
+    # Commit the changes and close the connection
+    df.to_sql(table_name, conn, if_exists="replace", index=False)
+    conn.commit()
+    conn.close()
+
+# Step 6: Main function
+def main():
+    # Set the file paths
+    db_path = "gtfs.sqlite"
+    zip_file_path = "GTFS.zip"
+
+    # Step 1: Download the GTFS ZIP file
+    download_zip_file("https://gtfs.rhoenenergie-bus.de/GTFS.zip", zip_file_path)
+    print("Downloaded GTFS data successfully.")
+
+    # Step 2: Unzip the file and get the stops.txt file path
+    unzip_file(zip_file_path, ".")
+    stops_file_path = os.path.join(".", "stops.txt")
+    print("Unzipped GTFS data successfully.")
+
+    # Step 3: Data transformation
+    df = data_transformation()
+    print("Data transformation completed.")
+
+    # Step 4: Data validation
+    df = data_validations(df)
+    print("Data validation completed.")
+
+    # Step 5: Load data
+    load_data(df, db_path, "stops")
+    print("Data loaded into SQLite database successfully.")
+
+    print("Data pipeline run completed.")
+
+# Main execution
+if __name__ == "__main__":
+    main()
