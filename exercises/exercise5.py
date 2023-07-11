62d7b30c53440b0473f512fea2873f47740fb227
theanindya <40605723+theanindya@users.noreply.github.com>
2023-07-11 21:25:18 +0200

    Update exercise5.py

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,27 +1,38 @@
-import pandas as pd
-import sqlalchemy as sa
+import os
 import urllib.request
 import zipfile
+import pandas as pd
+import sqlite3
 
-# Import the data
-url = 'https://gtfs.rhoenenergie-bus.de/GTFS.zip'
-with urllib.request.urlopen(url) as response:
-    with zipfile.ZipFile(io.BytesIO(response.read())) as zip_ref:
-        with zip_ref.open('stops.txt') as file:
-            df = pd.read_csv(file, sep=',', encoding='UTF-8', header=0, usecols=[0,2,4,5,6],names=["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"], dtype={"stop_id": int, "stop_name": str, "stop_lat": float, "stop_lon":float, "zone_id": int})
+# Set the file paths
+db_path = "gtfs.sqlite"
+zip_file_path = "GTFS.zip"
+csv_file_path = "stops.txt"
 
-# Filter the data
-df = df[df['zone_id'] != 2001]
+# Step 1: Download the ZIP file
+download_zip_file("https://gtfs.rhoenenergie-bus.de/GTFS.zip", zip_file_path)
 
-# Validate the data
+# Step 2: Unzip the file and get the CSV file path
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(".")
+
+# Step 3: Read the CSV file and create a Pandas DataFrame
+df = pd.read_csv(csv_file_path, sep=',', encoding='UTF-8', header=0, usecols=[0, 2, 4, 5, 6],dtype={"stop_id": int, "stop_name": str, "stop_lat": float, "stop_lon":float, "zone_id": int})
+
+# Step 4: Filter the data to only keep stops from zone 2001
+df = df[df['zone_id'] == 2001]
+
+# Step 5: Validate the data
 invalid_lats = df[df['stop_lat'] < -90]
 invalid_lons = df[df['stop_lon'] < -90]
 invalid_rows = invalid_lats.append(invalid_lons)
 df = df.drop(invalid_rows.index)
 
-# Drop NaN values
-df = df.dropna()
+# Step 6: Write the data to the SQLite database
+conn = sqlite3.connect(db_path)
+df.to_sql('stops', conn, if_exists="replace", index=False)
+conn.commit()
+conn.close()
 
-# Save the data to a SQL database
-engine = sa.create_engine('sqlite:///./gtfs.sqlite')
-df.to_sql('stops', con=engine, index=False)
+# Print a message to indicate that the data pipeline has run successfully
+print("Data pipeline runs successfully.")
